9f02356141d3c9f31e0981046ba8f4c0d79a3902
Lionel_NKEOUA <mbembasachat9@gmail.com>
2020-07-23 08:10:29 +0000

    redux lifecycle implemente

--- src/index.js
+++ src/index.js
@@ -3,9 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+  
 
 
-//Store
+//Store (State Global)
+
+//Action INCREMENt (This describe what I'll do)
+const increment = () => {
+
+  return {
+    type : "INCREMENT"
+  }
+}
+
+const decrement = () => {
+
+  return {
+    type : "DECREMENT"
+  }
+}
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch(action.type){
+      case 'INCREMENT':
+        return state + 1;
+
+      case 'DECREMENT':
+        return state - 1;  
+  }
+};
+
+let sotre = createStore(counter);
+
+//DISPACTH
 
 
 ReactDOM.render(
